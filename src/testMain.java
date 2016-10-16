5410c22a1faeeb4e149522a10204c1adde16495e
fankai <cgfankai@qq.com>
2016-10-16 23:03:12 +0800

    add the solution of 412

--- src/testMain.java
+++ src/testMain.java
@@ -2,4 +2,8 @@
  * Created by fankai on 2016/10/12.
  */
 public class testMain {
+    public static void main(String[] args){
+        char[] a = new char[21];
+        System.out.print(a[0]);
+    }
 }
