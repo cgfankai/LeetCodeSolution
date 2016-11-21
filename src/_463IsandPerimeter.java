0f052bbe0a5b29eadb971600b423e97875e6ce24
fankai <cgfankai@qq.com>
2016-11-21 21:15:34 +0800

    _463

--- src/_463IsandPerimeter.java
+++ src/_463IsandPerimeter.java
@@ -7,9 +7,9 @@ public class _463IsandPerimeter {
         for (int i = 0 ; i < grid.length ; i ++) {
             for (int j = 0 ; j < grid[i].length ; j ++ ) {
                 if(grid[i][j] == 1) {
-                    if(i == 0 || i == grid.length - 1)
+                    if(i == 0 )
                         perimeter++;
-                    if (j == 0 || j == grid[i].length - 1)
+                    if (j == 0 )
                         perimeter++;
                     if(i == grid.length - 1)
                         perimeter++;
