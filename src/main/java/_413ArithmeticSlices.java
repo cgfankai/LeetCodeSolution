114ed0085f18e8d3187f618986aad83c0e738662
fankai <cgfankai@qq.com>
2016-11-27 12:13:46 +0800

    413

--- src/main/java/_413ArithmeticSlices.java
+++ src/main/java/_413ArithmeticSlices.java
@@ -5,7 +5,16 @@ package main.java;
  */
 public class _413ArithmeticSlices {
     public int numberOfArithmeticSlices(int[] A) {
-
-        return 0;
+        if (A == null || A.length < 3)
+            return 0;
+        int sumOfArithmeticSlice = 0;
+        int numberOfElement = 0;
+        for (int i = 2 ; i < A.length; i++){
+            if (A[i] - A[i - 1] == A[i-1] - A[i-2]){
+                numberOfElement++;
+                sumOfArithmeticSlice += numberOfElement;
+            }else numberOfElement = 0;
+        }
+        return sumOfArithmeticSlice;
     }
 }
