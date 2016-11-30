86bcf49e9fbb0e484ea2834a1bfc5a7a8d850de8
fankai <cgfankai@qq.com>
2016-11-30 22:49:16 +0800

    382

--- src/main/java/_382LinkedListRandomNode.java
+++ src/main/java/_382LinkedListRandomNode.java
@@ -2,24 +2,35 @@ package main.java;
 
 import main.java.Collections.ListNode;
 
+import java.util.Random;
+
 /**
  * Created by fankai on 2016/11/29.
  */
 public class _382LinkedListRandomNode {
-    ListNode head;
-    int count = 1;
+    public ListNode head;
+    public int count = 0;
     /** @param head The linked list's head.
     Note that the head is guaranteed to be not null, so it contains at least one node. */
     public _382LinkedListRandomNode(ListNode head) {
         this.head = head;
         for (ListNode i = head; i.next!=null;i = i.next){
-            count++;
+            this.count++;
         }
     }
 
     /** Returns a random node's value. */
     public int getRandom() {
-        return  (int)(Math.random() * count) + 1;
+        Random random = new Random();
+        int index =   0;
+        index = random.nextInt(this.count + 1);
+        ListNode temp = head;
+        for(int i = 0; i <= index; i ++ ){
+            if (i == index)
+                return temp.val;
+            else temp = temp.next;
+        }
+        return 0;
     }
 }
 /**
---
+++ src/test/_382LinkedListRandomNodeTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import main.java.Collections.ListNode;
+import main.java._382LinkedListRandomNode;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by fankai on 2016/11/30.
+ */
+public class _382LinkedListRandomNodeTest {
+    @Test
+    public void getRandom() throws Exception {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        _382LinkedListRandomNode _382LinkedListRandomNode = new _382LinkedListRandomNode(head);
+
+    }
+
+}
